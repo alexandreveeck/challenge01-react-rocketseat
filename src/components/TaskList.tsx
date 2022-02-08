4db991aea39f7a389ff6021f13df20f564874d71
Alexandre Veeck da Conceição <brivia.aconceicao.c@AES.COM>
2022-02-08 18:05:00 -0300

    Adjustment

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -32,7 +32,7 @@ export function TaskList() {
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
     const newTaskList = tasks.map(task => task.id === id ? {
-      ...task, isComplete: !isComplete
+      ...task, isComplete: !task.isComplete
     } : task);
     setTasks(newTaskList);
   }
