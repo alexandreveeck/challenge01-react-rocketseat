cbb41c41004a439d0208f07b138eee0d0a07e68b
Alexandre Veeck da Conceição <brivia.aconceicao.c@AES.COM>
2022-02-08 18:02:12 -0300

    Challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { TaskList } from './components/TaskList'
+import { TaskList } from './components/TaskList';
 import { Header } from "./components/Header";
-import './styles/global.scss'
 
+import './styles/global.scss'
 
 export function App() {
   return (
     <>
-      <Header />
-      <TaskList />
+        <Header />
+        <TaskList />
     </>
   )
 }
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-
 import '../styles/tasklist.scss'
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
@@ -13,17 +12,35 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [isComplete, setComplete] = useState(false);
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle === '')
+      return;
+    
+    const newTask: Task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTaskList = tasks.map(task => task.id === id ? {
+      ...task, isComplete: !isComplete
+    } : task);
+    setTasks(newTaskList);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks);
   }
 
   return (
